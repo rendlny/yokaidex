46070c513edfdf936d2a269fca246058f17bff13
joaopedrodcf <joaoferr93@gmail.com>
2019-02-22 22:29:44 +0000

    Updates

--- precache-manifest.6f33a86a66b773b0882f417ab46c9893.js
+++ precache-manifest.6f33a86a66b773b0882f417ab46c9893.js
@@ -8,11 +8,11 @@ self.__precacheManifest = [
     "url": "/static/js/main.9c200f56.chunk.js"
   },
   {
-    "revision": "f67027e7c0813d351bc1",
-    "url": "/static/js/1.f67027e7.chunk.js"
+    "revision": "fb2b8f62c7bae177a264",
+    "url": "/static/js/1.fb2b8f62.chunk.js"
   },
   {
-    "revision": "3c43980d2be3c51ad748a2d344fc2272",
+    "revision": "50c807da01581b8d830b3ad0a83404a4",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc3622513009413b53e73d7cf01adb5b.js"
+  "/precache-manifest.6f33a86a66b773b0882f417ab46c9893.js"
 );
 
 workbox.clientsClaim();
